distance calculation fixed

Social Distancing Detector/social_distance_monitor.py
@@ -22,8 +22,8 @@ model_path = os.path.join(MODEL_DIR,"yolov3.weights")
 config_path = os.path.join(MODEL_DIR,"yolov3.cfg")
 class_names_path = os.path.join(MODEL_DIR,"coco.names")
 
-LABELS = open(class_names_path).read().strip().split("\n")
-top_points,rect_points,top_view_img = calibrator(r"G:\TSF_Tasks\Social Distancing Detector\calibratecam.jpg")
+LABELS = open(class_names_path).read().strip().split("\n") #list of size num_class_names
+top_points,rect_points,top_view_img = calibrator(r"G:\TSF_Tasks\Social Distancing Detector\samples\calibratecam.jpg")
 matrix = eagle_eye(top_points,rect_points)
 
 
@@ -41,7 +41,7 @@ ln = [ln[i[0]-1] for i in net.getUnconnectedOutLayers()]
 print("[INFO] Processing the input...")
 vs = cv.VideoCapture(args["input"] if args["input"] else 0)
 writer = None
-writer_top = None
+#writer_top = None
 
 while True:
     (success,frame) = vs.read()
@@ -59,42 +59,52 @@ while True:
 
     if len(results)>1:
         centroids = np.array([result[2] for result in results])
-        distances = dist.cdist(centroids,centroids,metric="euclidean")
 
-        for i in range(distances.shape[0]):
-            for j in range(i+1,distances.shape[1]):
-                if distances[i][j]<MIN_DISTANCE:
-                    violate.add(i)
-                    violate.add(j)
+    dist_points = []
+    for (i, (conf, bbox, centroid)) in enumerate(results):
+        (c1,c2) = centroid
+        px,py = top_plotter(c1,c2,matrix)
+        dist_points.append([px,py])
+    
+    distances = dist.cdist(dist_points,dist_points,metric="euclidean")
+
+    for i in range(distances.shape[0]):
+        for j in range(i+1,distances.shape[1]):
+            if distances[i][j]<MIN_DISTANCE:
+                violate.add(i)
+                violate.add(j)
+
+    for (i,point_top) in enumerate(dist_points):
+        px,py = point_top
+        color = (0,255,0)
+        if i in violate:
+            color = (0,0,255)
+        cv.circle(top_view,(px,py),5,color,-1)
 
     for (i, (conf, bbox, centroid)) in enumerate(results):
         (x1,y1,x2,y2) = bbox
         (c1,c2) = centroid
-        px,py = top_plotter(c1,c2,matrix)
         color = (0,255,0)
-
         if i in violate:
             color = (0,0,255)
-
         cv.rectangle(frame,(x1,y1),(x2,y2),color,2)   
         cv.circle(frame,(c1,c2),3,color,1)
-        cv.circle(top_view,(px,py),5,color,-1)
-
-    
+  
+    final = np.concatenate((frame,top_view),axis=1)
     text = "Total Violations:{}".format(len(violate))
-    cv.putText(frame,text,(0,70),cv.FONT_HERSHEY_SIMPLEX,0.85,(255,0,0),2)
+    cv.putText(final,text,(0,70),cv.FONT_HERSHEY_SIMPLEX,0.85,(255,0,0),2)
 
     if args["display"]>0:
-        cv.imshow("Video",top_view)
+        cv.imshow("Video",final)
         key = cv.waitKey(1) & 0xFF
         if key == ord("q"):
             break
 
     if args["output"] != "" and writer is None:
         fourcc = cv.VideoWriter_fourcc(*"MJPG")
-        writer = cv.VideoWriter(args["output"], fourcc, 25,(frame.shape[1], frame.shape[0]), True)
-        writer_top = cv.VideoWriter(r"G:\TSF_Tasks\Social Distancing Detector\final_output_top.avi", fourcc, 25,(top_view.shape[1],top_view.shape[0]), True)
+        writer = cv.VideoWriter(args["output"], fourcc, 25,(final.shape[1], final.shape[0]), True)
+        #writer_top = cv.VideoWriter(r"G:\TSF_Tasks\Social Distancing Detector\final_output_top.avi", fourcc, 25,(top_view.shape[1],top_view.shape[0]), True)
 
     if writer is not None:
-        writer.write(frame)
-        writer_top.write(top_view)
+        writer.write(final)
+        #writer_top.write(top_view)

Social Distancing Detector/testimage.py
@@ -6,7 +6,7 @@ from utils.params import MODEL_DIR
 from scipy.spatial import distance as dist
 import os
 
-img = cv.imread(r"G:\TSF_Tasks\Social Distancing Detector\calibratecam.jpg")
+img = cv.imread(r"G:\TSF_Tasks\Social Distancing Detector\samples\calibratecam.jpg")
 (H,W) = img.shape[:2]
 
 pts1 = np.float32([[int(0.22*W),int(0.44*H)],[int(0.36*W),int(0.42*H)],[int(W),int(0.8*H)],[int(0.7*W),int(H)]])
@@ -68,4 +68,6 @@ cv.putText(img,text,(0,0),cv.FONT_HERSHEY_SIMPLEX,0.85,(255,0,0),2)
 
 cv.imshow("Image",img)
 cv.imshow("Top_view",top_view)
+cv.imwrite(r"img1.jpg",img)
+cv.imwrite(r"img2.jpg",top_view)
 cv.waitKey(0)